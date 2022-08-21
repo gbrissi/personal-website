838e507a5f1f03602d094615487dda5665c140b3
Gabriel Rissi <gbrissi123@gmail.com>
2022-08-21 18:49:08 -0300

    style: updating handshake style

--- portfolio/src/components/Banner.tsx
+++ portfolio/src/components/Banner.tsx
@@ -14,7 +14,7 @@ function Banner() {
             <Typography variant='body1'>Software development student with experience in JavaScript, NodeJS and React among many other libraries and frameworks :D</Typography>
           </BannerComponents>
           <BannerComponents sx={{flexDirection: 'row'}}>
-            <CustomTypography variant='h2'>Hello there, my name is Gabriel! &#128075;</CustomTypography>
+            <CustomTypography variant='h2'>Hello there, my name is Gabriel! <span style={{position: 'relative', fontSize:'3rem', top: '-5px'}}>&#128075;</span></CustomTypography>
           </BannerComponents>
         </BannerContainer>
       </Wrapper>
